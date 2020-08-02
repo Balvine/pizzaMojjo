a1061eb591965f139fe407c78bf989a34ca3c0d4
Balvine <balvy075@gmail.com>
2020-08-02 22:25:47 +0300

    added output functions

--- js/script.js
+++ js/script.js
@@ -9,3 +9,8 @@ var priceForm = document.getElementById("priceForm");
 //pizza output  for the table
 var pizzaOutput = document.getElementById("pizzaOutput");
 var crustOutput = document.getElementById("crustOutput");
+var toppingOutput = document.getElementById("toppingOutput");
+var quantityOutput = document.getElementById("quantityOutput");
+var deliveryOutput = document.getElementById("deliveryOutput");
+
+var total amount = document.getElementById("totals");
