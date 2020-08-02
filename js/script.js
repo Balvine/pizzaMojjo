4ebf90fc65887ebc7e55b251cf32647828499488
Balvine <balvy075@gmail.com>
2020-08-03 00:03:36 +0300

    added alert function

--- js/script.js
+++ js/script.js
@@ -1,47 +1,42 @@
-//pizza input in the forms
+//=>pizza input
 var pizzaSize = document.getElementById("pizzaSize");
 var pizzaCrust = document.getElementById("pizzaCrust");
 var pizzaTopping = document.getElementById("pizzaTopping");
 var pizzaNumber = document.getElementById("pizzaNumber");
+var pizzaLocation = document.getElementById("pizzaLocation");
 
 var priceForm = document.getElementById("priceForm");
 
-//pizza output  for the table
+//=>pizza output
 var pizzaOutput = document.getElementById("pizzaOutput");
 var crustOutput = document.getElementById("crustOutput");
 var toppingOutput = document.getElementById("toppingOutput");
 var quantityOutput = document.getElementById("quantityOutput");
 var deliveryOutput = document.getElementById("deliveryOutput");
+var totals = document.getElementById("totals");
 
-var totals= document.getElementById("totals");
-
-//calculations
-priceForm.addEventListener("submit",priceCalc);
-
+//=>add functionality
+priceForm.addEventListener("submit", priceCalc);
 
 function priceCalc(e) {
-    e.preventDefault();
-    
-    if (
+  e.preventDefault();
+
+  if (
     pizzaSize === "" ||
     pizzaCrust === "" ||
     pizzaTopping === "" ||
     pizzaNumber === ""
-    ) {
+  ) {
     alert("Fill in the required inputs. (starred*)");
-    } else {
+  } else {
     pizzaOutput.innerHTML = `${pizzaSize.value}`;
     crustOutput.innerHTML = `${pizzaCrust.value}`;
     toppingOutput.innerHTML = `${pizzaTopping.value}`;
     quantityOutput.innerHTML = `${pizzaNumber.value}`;
     deliveryOutput.innerHTML = `${pizzaLocation.value}`;
     totals.innerHTML =
-    (+pizzaSize.value + +pizzaCrust.value + +pizzaTopping.value) *
-    pizzaNumber.value +
-    +pizzaLocation.value;
-    }
-    deliveryOutput{
-        alert("your order will be delivered to your location");
-    }
-    }
-    
+      (+pizzaSize.value + +pizzaCrust.value + +pizzaTopping.value) *
+        pizzaNumber.value +
+      +pizzaLocation.value;
+  }
+}
