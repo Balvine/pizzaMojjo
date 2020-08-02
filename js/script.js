2687267c339245134c3f1409e8ff3c4a81e758bf
Balvine <balvy075@gmail.com>
2020-08-02 21:50:24 +0300

    added input functions

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//pizza input in the forms
+var pizzaSize = document.getElementById("pizzaSize");
+var pizzaCrust = document.getElementById("pizzaCrust");
+var pizzaTopping = document.getElementById("pizzaTopping");
+var pizzaNumber = document.getElementById("pizzaNumber";
